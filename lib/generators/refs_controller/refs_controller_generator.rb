1fbf656ee88ab00c8568f234ef672e6185e3b3ab
Максим Третьяков <max@tretyakov-ma.ru>
2016-04-26 21:09:33 +0500

    generators

--- lib/generators/refs_controller/refs_controller_generator.rb
+++ lib/generators/refs_controller/refs_controller_generator.rb
@@ -1,8 +1,13 @@
+require 'rails/generators'
+
 class RefsControllerGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def create_controllers
     @name = name.underscore
-    template 'refs_controller.rb', "app/controllers/#{@name}_controller.rb"
+
+    template 'refs_controller.rb.template', "app/controllers/#{@name}_controller.rb"
+    directory 'views', "app/views/#{@name}"
+    directory 'locales', 'config/locales'
   end
 end
